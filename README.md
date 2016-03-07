d99a22e32384f02c48fb6b1cd07f603e406dd8c3
BoatingZeng <945467442@qq.com>
2016-03-07 16:45:29 +0800

    add many files.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # boatgal
 Web gal engine using createjs.
+做不出来吃键盘,新买的。
---
+++ asset/customTitle.js
@@ -0,0 +1,7 @@
+/**
+ * 定制的游戏标题画面,需要以某种形式调用beginScenario,以进入游戏主体
+ * @method customTitle
+ */
+ boatgal.customTitle = function() {
+  console.log('this is customTitle');
+ };
